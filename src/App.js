e385961d53c1e140ae0a17fd56d65eac235acd58
Roei Malka <roeimalka@Roeis-MacBook-Pro.local>
2020-03-19 11:21:54 +0200

    feat: add route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import List from './List.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <List></List>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/list">List</Link>
+          </li>
+        </ul>
+
+        <hr />
+        <Switch>
+          <Route exact path="/">
+          </Route>
+          <Route path="/list">
+            <List />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/List.js
+++ src/List.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Footer() {
+function List() {
   return (
     <div>List Component</div>
   )
 }
 
-export default Footer;
+export default List;
